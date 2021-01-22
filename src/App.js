16c5fb7320f2c55efe370ce7b06c6b449bdd0789
Prateek <rprateek254@gmail.com>
2021-01-22 19:15:07 +0530

    added links and multiple pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import { Switch, Route } from 'react-router';
+import Navs from './components/Navs';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path="/">
-        This is home page.
-      </Route>
-      <Route exact path="/abc">
-        This is abc.
-      </Route>
-      <Route>Page Not Found 404</Route>
-    </Switch>
+    <div>
+      <Navs />
+
+      <Switch>
+        <Route exact path="/">
+          This is home page.
+        </Route>
+        <Route exact path="/abc">
+          This is abc.
+        </Route>
+        <Route>Page Not Found 404</Route>
+      </Switch>
+    </div>
   );
 }
 
---
+++ src/components/Navs.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+// eslint-disable-next-line
+const Navs = () => {
+  return (
+    <div>
+      <ul>
+        <Link to="/abc">Go to abc</Link>
+      </ul>
+    </div>
+  );
+};
+
+export default navs;
