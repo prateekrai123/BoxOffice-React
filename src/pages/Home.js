275eacc62d76c6a8334192a9b84bd2910e680051
Prateek <rprateek254@gmail.com>
2021-01-25 13:17:45 +0530

    added search bar and button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
 
 // eslint-disable-next-line
 const Home = () => {
-  return <MainPageLayout>This is home.</MainPageLayout>;
+  const [input, setInput] = useState('');
+
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onSeach = () => {};
+
+  return (
+    <MainPageLayout>
+      <input type="text" onChange={onInputChange} value={input} />
+      <button type="button" onClick={onSeach}>
+        Search
+      </button>
+    </MainPageLayout>
+  );
 };
 
 export default Home;
