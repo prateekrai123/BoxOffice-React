fead4d3b167b0734617eefb3d892405906041c5b
Prateek <rprateek254@gmail.com>
2021-01-27 14:55:06 +0530

    displayed show results and minor fetch refactor

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import MainPageLayout from '../components/MainPageLayout';
+import { apiGet } from '../misc/config';
 
 // eslint-disable-next-line
 const Home = () => {
@@ -7,12 +8,9 @@ const Home = () => {
   const [results, setResults] = useState(null);
 
   const onSearch = () => {
-    fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
-      .then(r => r.json())
-      .then(result => {
-        setResults(result);
-        console.log(result);
-      });
+    apiGet(`search/shows?q=${input}`).then(result => {
+      setResults(result);
+    });
   };
 
   const onInputChange = ev => {
@@ -25,9 +23,23 @@ const Home = () => {
     }
   };
 
-  const renderResults = () = {
+  const renderResults = () => {
+    if (results && results.length === 0) {
+      return <div>No results</div>;
+    }
 
-  }
+    if (results && results.length > 0) {
+      return (
+        <div>
+          {results.map(item => (
+            <div key={item.show.id}>{item.show.name}</div>
+          ))}
+        </div>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <MainPageLayout>
