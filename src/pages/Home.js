4530aca60c146676fdc56f4fe3cac00f1741389b
Prateek <rprateek254@gmail.com>
2021-01-27 11:43:03 +0530

    handled enter button event

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,11 +5,7 @@ import MainPageLayout from '../components/MainPageLayout';
 const Home = () => {
   const [input, setInput] = useState('');
 
-  const onInputChange = ev => {
-    setInput(ev.target.value);
-  };
-
-  const onSeach = () => {
+  const onSearch = () => {
     fetch(`http://api.tvmaze.com/search/shows?q=${input}`)
       .then(r => r.json())
       .then(result => {
@@ -17,10 +13,25 @@ const Home = () => {
       });
   };
 
+  const onInputChange = ev => {
+    setInput(ev.target.value);
+  };
+
+  const onKeyDown = ev => {
+    if (ev.keyCode === 13) {
+      onSearch();
+    }
+  };
+
   return (
     <MainPageLayout>
-      <input type="text" onChange={onInputChange} value={input} />
-      <button type="button" onClick={onSeach}>
+      <input
+        type="text"
+        onChange={onInputChange}
+        onKeyDown={onKeyDown}
+        value={input}
+      />
+      <button type="button" onClick={onSearch}>
         Search
       </button>
     </MainPageLayout>
