f6c986698005d51429ea45a8baba52f85a033dc0
Prateek <rprateek254@gmail.com>
2021-01-28 19:13:56 +0530

    added actor preview cards

--- src/components/actor/ActorGrid.js
+++ src/components/actor/ActorGrid.js
@@ -1,12 +1,22 @@
 import React from 'react';
 import ActorCard from './ActorCard';
+import NOT_FOUND_IMAGE from '../../images/not-found.png';
 
 // eslint-disable-next-line
 const ActorGrid = ({ data }) => {
   return (
     <div>
       {data.map(({ person }) => (
-        <ActorCard key={person.id} id={person.id} name={person.name} />
+        <ActorCard
+          key={person.id}
+          name={person.name}
+          country={person.country ? person.country.name : null}
+          birthday={person.birthday}
+          deathday={person.deathday}
+          gender={person.gender}
+          image={person.image ? person.image.medium : NOT_FOUND_IMAGE}
+          summary={person.summary}
+        />
       ))}
     </div>
   );
